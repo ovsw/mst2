e3a0bb0c9df986d6aa064c0c9d9f2fd97bfe1528
Ovi Savescu <ovi@ovswebsites.com>
2018-02-15 15:17:06 +0200

    built responsive up to desktop v1

--- home.php
+++ home.php
@@ -22,32 +22,34 @@
       </div>
       <!-- /.hero__bg-wrapper -->
       
-      
+      <div class="hero__cards-wrapper">
+        <div class="hero__cards">
+          
 
+        
       <?php 
       $i = 0;
       foreach ($page->landing_cards as $card) : $i++; ?>      
-      
-      <div class="hero__card-wrapper">
-        <div class="hero__card n-<?= $i ?>">
-          <div class="hero__card-text n-<?= $i ?>">
-          
-            <h2 class="hero__card-title"><?= $card->title ?></h2>
-            <div class="hero__card-icon" style="background-image: url('<?= $card->card_icon->url ?>')"></div>
-            <div class="hero__card-description n-<?= $i ?>"><?= $card->card_short_desc ?></div>
-            <a class="button hero__card-button n-<?= $i ?>" href="<?= $card->read_more_link ?>">Learn more</a>
-          <a class="button hero__card-button n-<?= $i ?>" href="<?= $card->cta_link ?>">Book us</a>
-          </div>
-          <!-- /.hero__card-text -->
+        
+            <div class="hero__card n-<?= $i ?>">
+              <div class="hero__card-text n-<?= $i ?>">
+              
+                <h2 class="hero__card-title"><?= $card->title ?></h2>
+                <div class="hero__card-icon" style="background-image: url('<?= $card->card_icon->url ?>')"></div>
+                <div class="hero__card-description n-<?= $i ?>"><?= $card->card_short_desc ?></div>
+                <a class="button hero__card-button n-<?= $i ?>" href="<?= $card->read_more_link ?>">Learn more</a>
+              <a class="button hero__card-button n-<?= $i ?>" href="<?= $card->cta_link ?>">Book us</a>
+              </div>
+              <!-- /.hero__card-text -->
 
-          
-        </div>
-        <!-- /.hero__card -->
-      </div>
-      <!-- /. hero__card-wrapper -->
-
-
+            </div>
+            <!-- /.hero__card -->
+       
       <?php endforeach; ?>
+          </div>
+          <!-- /.hero__cards-wrapper -->
+        </div>
+        <!-- /.hero__cards -->
 
       
       <div class="hero__left-curtain"></div>
@@ -55,37 +57,42 @@
     
     </section>
 
+    <div class="footer__wrapper">
     <footer class="footer">
+      <div class="footer__inner-wrapper">
         
-      <div class="footer__logos">
-        <a href="#" class="footer__logo-link"><img src="<?= $config->urls->templates ?>assets/images/nystea-logo.png" alt="Mainstages Logo"></a>
-        <a href="#" class="footer__logo-link"><img src="<?= $config->urls->templates ?>assets/images/mainstages-logo.png" alt="Mainstages Logo"></a>
-      </div>
+        <div class="footer__logos">
+          <a href="#" class="footer__logo-link"><img src="<?= $config->urls->templates ?>assets/images/nystea-logo.png" alt="Mainstages Logo"></a>
+          <a href="#" class="footer__logo-link"><img src="<?= $config->urls->templates ?>assets/images/mainstages-logo.png" alt="Mainstages Logo"></a>
+        </div>
 
-      <div class="footer__row">
+        <div class="footer__row">
 
-        <div class="footer__column footer__column--1">
-          <div class="adr">
-            <div class="street-address">21-33 29th. Avenue - Suite 2</div>
-            <div>
-              <span class="locality">Astoria</span>, <span class="region" title="New York">NY</span>
-              <span class="postal-code">11102</span>
+          <div class="footer__column footer__column--1">
+            <div class="adr">
+              <div class="street-address">21-33 29th. Avenue - Suite 2</div>
+              <div>
+                <span class="locality">Astoria</span>, <span class="region" title="New York">NY</span>
+                <span class="postal-code">11102</span>
+              </div>
+              <div class="country-name">USA</div>
             </div>
-            <div class="country-name">USA</div>
+          </div>
+          
+          <div class="footer__column footer__column--2">
+            <div>Phone: <a class="tel" href="tel:+1-347-878-2431">347-878-2431</a></span></div>
+            <div>Email: <a href="mailto:support@mainstages.com" class="email">support@mainstages.com</a></div>
+            <div class="tel"><span class="type">Fax</span>:
+            <span class="value">+1-111-111-111</span></div>
           </div>
         </div>
-        
-        <div class="footer__column footer__column--2">
-          <div>Phone: <a class="tel" href="tel:+1-347-878-2431">347-878-2431</a></span></div>
-          <div>Email: <a href="mailto:support@mainstages.com" class="email">support@mainstages.com</a></div>
-          <div class="tel"><span class="type">Fax</span>:
-          <span class="value">+1-111-111-111</span></div>
-        </div>
+
+        <p class="footer__copyright">Copyright &copy;<?= date("Y") ?> Mainstages. All Rights Reserved | Website by <a href="http://ovswebsites.com/">OVS Websites</a></p>
       </div>
-
-      <p class="footer__copyright">Copyright &copy;<?= date("Y") ?> Mainstages. All Rights Reserved | Website by <a href="http://ovswebsites.com/">OVS Websites</a></p>
-
+      <!-- /.footer__inner-wrapper -->
     </footer>
+    </div>
+    <!-- /.footer__wrapper -->
 
     <?php if($page->editable()) echo "<p><a href='$page->editURL'>Edit</a></p>"; ?>
 
