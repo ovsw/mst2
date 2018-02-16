9216ed5c7b6eab6693788022c474547000c6f337
Ovi Savescu <ovi@ovswebsites.com>
2018-02-16 09:31:54 +0200

    basic html structure with nav

--- main-performances.php
+++ main-performances.php
@@ -1,97 +1,66 @@
 <!doctype html>
 <html class="no-js" lang="en">
-  <?php require($config->paths->templates . 'includes/head.inc');  ?>
+  <?php require($config->paths->templates . 'includes/head-performances.inc');  ?>
   <body>
     <!--[if lte IE 9]>
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
     <![endif]-->
 
-
-<div  class="nav-icon"><span></span><span></span><span></span><span></span></div>
-<div id="sideNav" class="sidenav">
-  <a href="#">About Us</a>
-  <a href="#">Staff</a>
-  <a href="#">Families</a>
-  <a href="#">Enroll Now</a>
-  <a href="#">Contact Us</a>
-</div>
-
-<!-- Use any element to open the sidenav -->
-
-
-<!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
-<div class="mainWrapper">
-     <header class="mainHeader">
-      
-      <a href="/" class="main-logo"><img src="<?= $config->urls->templates ?>images/oasis-children-logo.svg" alt="Oasis Day Camp logo"></a>
-      
-      <nav role="navigation" class="mainNavWrapper">
-        <ul class="mainNav">
-          <li>
-            <a href="#">About Us</a>
-            <ul>
-              <li><a href="#">1</a></li>
-              <li><a href="#">2</a></li>
-              <li><a href="#">3</a></li>
-            </ul>
-          </li>
-          <li><a href="#">Staff</a></li>
-          <li><a href="#">Families</a></li>
-          <li><a href="#">Enroll Now</a></li>
-          <li><a href="#">Contact Us</a></li>
-        </ul>
-      </nav>
-
-     </header>
-
-    <section>
-      <div class="heroWrapper">
-
-        <div class="heroContent">
-
-          <div class="heroMainColumn">
-            nothing
-          </div>
-          <!-- /.hpHeroMainColumn -->
-          <div class="heroMainColumn bottomCards">
-            <?php for ($i=0; $i < 10 ; $i++) : ?>
-            <div class="heroCardWrapper">
-              <div class="card">
-                <div class="cardFront">
-                  <h2 class="cardTitle">Link Title</h2>
-                  <p class="cardSubtitle">646-698-1800 <br> Location info here</p>
-                </div>
-                <!-- /.cardFront -->
-                <div class="cardBack">
-                  <h2 class="cardTitle">Card Back</h2>
-                  <p class="cardSubtitle">646-698-1800 <br> Location info here</p>
-                </div>
-                <!-- /.cardBack -->
-              </div>
-              <!-- /.card -->
-            </div>
-            <!-- /.heroCardBlock -->
-            <?php endfor ?>
-          </div>
-          <!-- /.hpHeroMainColumn -->
-
-        </div>
-        <!-- /.heroContent -->
-        
-        <div class="heroVideoBg">
-          <video src="http://<?= $config->httpHost ?><?= $config->urls->templates ?>video/oasis-children-all-camps-intro.mp4" autoplay loop mute>
-          </video>
-        </div>
-        <!-- /.heroVideoBg -->
-       
+    <header class="header">
+      <div class="header__wrapper">
+        <a href="/" class="header__logo">
+          <img src="<?= $config->urls->templates ?>assets/images/mainstages-logo-horizontal.png" alt="Mainstages Logo"> 
+        </a>
+        <?php require($config->paths->templates."includes/modules/topnav-performances.inc"); ?>
       </div>
-      <!-- /.heroWrapper -->
+      <!-- /.header__wrapper -->
+    </header>
+
+
+    <section class="hero">
+      <h1 class="hero__heading">Our Performances</h1>
+      <p class="hero__hookline">Hi, I'm Miles! Thre are over 30 shows for you to choose from. Use the performance wizard to find the right one for your needs.</p>
+      <a class="button hero__cta" href="#">find your performance -></a>
     </section>
-     
-     <section>
-       <h3>hp content</h3>
-     </section>
-</div>
+
+
+    <div class="footer__wrapper">
+    <footer class="footer">
+      <div class="footer__inner-wrapper">
+        
+        <div class="footer__logos">
+          <a href="#" class="footer__logo-link"><img src="<?= $config->urls->templates ?>assets/images/nystea-logo.png" alt="Mainstages Logo"></a>
+          <a href="#" class="footer__logo-link"><img src="<?= $config->urls->templates ?>assets/images/mainstages-logo.png" alt="Mainstages Logo"></a>
+        </div>
+
+        <div class="footer__row">
+
+          <div class="footer__column footer__column--1">
+            <div class="adr">
+              <div class="street-address">21-33 29th. Avenue - Suite 2</div>
+              <div>
+                <span class="locality">Astoria</span>, <span class="region" title="New York">NY</span>
+                <span class="postal-code">11102</span>
+              </div>
+              <div class="country-name">USA</div>
+            </div>
+          </div>
+          
+          <div class="footer__column footer__column--2">
+            <div>Phone: <a class="tel" href="tel:+1-347-878-2431">347-878-2431</a></span></div>
+            <div>Email: <a href="mailto:support@mainstages.com" class="email">support@mainstages.com</a></div>
+            <div class="tel"><span class="type">Fax</span>:
+            <span class="value">+1-111-111-111</span></div>
+          </div>
+        </div>
+
+        <p class="footer__copyright">Copyright &copy;<?= date("Y") ?> Mainstages. All Rights Reserved | Website by <a href="http://ovswebsites.com/">OVS Websites</a></p>
+      </div>
+      <!-- /.footer__inner-wrapper -->
+    </footer>
+    </div>
+    <!-- /.footer__wrapper -->
+
   <?php require($config->paths->templates . 'includes/foot-performances.inc');  ?>
   </body>
 </html>
