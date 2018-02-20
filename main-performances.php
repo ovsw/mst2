27507cca0302f7e794deb6e01799408f12789fc8
Ovi Savescu <ovi@ovswebsites.com>
2018-02-20 14:07:29 +0200

    made performances footer modular

--- main-performances.php
+++ main-performances.php
@@ -62,42 +62,7 @@
     <!-- /.slidecards -->
 
 
-    <div class="footer__wrapper">
-    <footer class="footer">
-      <div class="footer__inner-wrapper">
-        
-        <div class="footer__logos">
-          <a href="#" class="footer__logo-link"><img src="<?= $config->urls->templates ?>assets/images/nystea-logo.png" alt="Mainstages Logo"></a>
-          <a href="#" class="footer__logo-link"><img src="<?= $config->urls->templates ?>assets/images/mainstages-logo.png" alt="Mainstages Logo"></a>
-        </div>
-
-        <div class="footer__row">
-
-          <div class="footer__column footer__column--1">
-            <div class="adr">
-              <div class="street-address">21-33 29th. Avenue - Suite 2</div>
-              <div>
-                <span class="locality">Astoria</span>, <span class="region" title="New York">NY</span>
-                <span class="postal-code">11102</span>
-              </div>
-              <div class="country-name">USA</div>
-            </div>
-          </div>
-          
-          <div class="footer__column footer__column--2">
-            <div>Phone: <a class="tel" href="tel:+1-347-878-2431">347-878-2431</a></span></div>
-            <div>Email: <a href="mailto:support@mainstages.com" class="email">support@mainstages.com</a></div>
-            <div class="tel"><span class="type">Fax</span>:
-            <span class="value">+1-111-111-111</span></div>
-          </div>
-        </div>
-
-        <p class="footer__copyright">Copyright &copy;<?= date("Y") ?> Mainstages. All Rights Reserved | Website by <a href="http://ovswebsites.com/">OVS Websites</a></p>
-      </div>
-      <!-- /.footer__inner-wrapper -->
-    </footer>
-    </div>
-    <!-- /.footer__wrapper -->
+    <?php require($config->paths->templates."includes/footer-performances.inc") ?>
 
   <?php require($config->paths->templates . 'includes/foot-performances.inc');  ?>
   </body>
