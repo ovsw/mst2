55a4c8583f871a4287b2344a9fe772fbc77c23df
Ovi Savescu <ovi@ovswebsites.com>
2018-02-28 15:56:41 +0200

    updated FAQs to use object syntax

--- assets/scripts/common-bundle.js
+++ assets/scripts/common-bundle.js
@@ -11573,12 +11573,40 @@ window.$ = __WEBPACK_IMPORTED_MODULE_0_jquery___default.a;
 
 // toggle FAQs
 
-$('.faqs__answer').hide();
-$('.faqs__question').click(function() {
-  var toggle = $(this).nextUntil('.faqs__question');
-  toggle.slideToggle();
-  $('.faqs__answer').not(toggle).slideUp();
-});
+var Faqs = {
+
+  init : function() {
+
+    this.cachedom();
+    this.bindEvents();
+
+  },
+
+  cachedom: function() {
+    this.$questions = $('.faqs__question');
+    this.$answers = $('.faqs__answer');
+  },
+
+  bindEvents: function() {
+    this.$questions.on('click', this.hideAllShowCurrent);
+  },
+
+  hideAllShowCurrent: function() {
+    var toggle = $(this).nextUntil('.faqs__question');
+    toggle.slideToggle();
+    Faqs.$answers.not(toggle).slideUp();
+  }
+
+};
+
+Faqs.init();
+
+// $('.faqs__answer').hide();
+// $('.faqs__question').click(function() {
+//   var toggle = $(this).nextUntil('.faqs__question');
+//   toggle.slideToggle();
+//   $('.faqs__answer').not(toggle).slideUp();
+// });
 
 /***/ })
 /******/ ]);
--- sources/scripts/modules/faqs.js
+++ sources/scripts/modules/faqs.js
@@ -1,8 +1,36 @@
 // toggle FAQs
 
-$('.faqs__answer').hide();
-$('.faqs__question').click(function() {
-  var toggle = $(this).nextUntil('.faqs__question');
-  toggle.slideToggle();
-  $('.faqs__answer').not(toggle).slideUp();
-});
+var Faqs = {
+
+  init : function() {
+
+    this.cachedom();
+    this.bindEvents();
+
+  },
+
+  cachedom: function() {
+    this.$questions = $('.faqs__question');
+    this.$answers = $('.faqs__answer');
+  },
+
+  bindEvents: function() {
+    this.$questions.on('click', this.hideAllShowCurrent);
+  },
+
+  hideAllShowCurrent: function() {
+    var toggle = $(this).nextUntil('.faqs__question');
+    toggle.slideToggle();
+    Faqs.$answers.not(toggle).slideUp();
+  }
+
+};
+
+Faqs.init();
+
+// $('.faqs__answer').hide();
+// $('.faqs__question').click(function() {
+//   var toggle = $(this).nextUntil('.faqs__question');
+//   toggle.slideToggle();
+//   $('.faqs__answer').not(toggle).slideUp();
+// });
