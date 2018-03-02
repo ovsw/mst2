df9ac8d9a9d23c1b7fbb9dd9e16342572d6d0cc7
Ovi Savescu <ovi@ovswebsites.com>
2018-03-02 17:29:56 +0200

    added error proofing to CTA button matrix module

--- fields/modular_content/cta_button.php
+++ fields/modular_content/cta_button.php
@@ -1,2 +1,4 @@
 
-<p class="performance-cta-btn"><a class="button" href="<?= $page->page_selector_any->url ?>"><?= $page->title ?></a></p>
+<?php if ($page->page_selector_any && $page->title != '') : ?>
+<p class="performance-cta-btn"><a class="button" href="<?= $page->page_selector_any->url ?>"><?= $page->title ?></a></p>  
+<?php endif; ?>
