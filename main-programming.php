517dc816fe4a889fd648bd9abe77f3ff6606f6a3
Ovi Savescu <ovi@ovswebsites.com>
2018-03-08 16:16:01 +0200

    minified css

--- main-programming.php
+++ main-programming.php
@@ -1,32 +1,6 @@
 <!doctype html>
 <html class="no-js" lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title><?php echo $page->title; ?></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="manifest" href="site.webmanifest">
-    <link rel="apple-touch-icon" href="icon.png">
-    <!-- Place favicon.ico in the root directory -->
-
-    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
-    <script>
-    WebFontConfig = {
-      typekit: { id: 'bea5cur' }
-    };
-
-    (function(d) {
-        var wf = d.createElement('script'), s = d.scripts[0];
-        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-        wf.async = true;
-        s.parentNode.insertBefore(wf, s);
-     })(document);
-    </script>
-
-    <link rel="stylesheet" href="<?php echo $config->urls->templates?>assets/styles/styles-programming.css" />
-  </head>
+  <?php require($config->paths->templates . 'includes/head-programming.inc');  ?>
   <body>
     <!--[if lte IE 9]>
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
